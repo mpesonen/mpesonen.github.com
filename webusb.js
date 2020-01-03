e3710675e14452a8a254a7ed27b4a1c693d4c6b6
Matti <matti.h.pesonen@gmail.com>
2020-01-04 01:13:50 +0200

    Send some value fix

--- webusb.js
+++ webusb.js
@@ -1,50 +1,54 @@
 // (Global) variables
 var port;
-var MidiMessageType;
-(function (MidiMessageType) {
-    MidiMessageType[MidiMessageType["ControlChange"] = 0] = "ControlChange";
-    MidiMessageType[MidiMessageType["ProgramChange"] = 1] = "ProgramChange";
-    MidiMessageType[MidiMessageType["NoteOn"] = 2] = "NoteOn";
-})(MidiMessageType || (MidiMessageType = {}));
-var ButtonCommand = /** @class */ (function () {
-    function ButtonCommand(type, value) {
-        this.type = type;
-        this.value = value;
-        this.messageType = type;
-        this.messageValue = value;
-    }
-    return ButtonCommand;
-}());
-function sendButtonCommandUpdate(buttonIndex, buttonCommand) {
-    if (!port) {
-        return;
-    }
-    var buttonCommandUsbMessageData = new Uint8Array(3);
-    buttonCommandUsbMessageData[0] = buttonIndex;
-    buttonCommandUsbMessageData[1] = getMidiMessageTypeNumber(buttonCommand.messageType);
-    buttonCommandUsbMessageData[2] = getMidiMessageTypeNumber(buttonCommand.messageValue);
-    port.send(buttonCommandUsbMessageData);
-}
-function getMidiMessageTypeNumber(type) {
-    switch (type) {
-        case MidiMessageType.ControlChange: {
-            return parseInt("0xB0");
-            break;
-        }
-        case MidiMessageType.ProgramChange: {
-            return parseInt("0xC0");
-            break;
-        }
-        case MidiMessageType.NoteOn: {
-            return parseInt("0x90");
-            break;
-        }
-    }
-}
 window.onload = function () {
     console.log("ONLOAD");
     var connectButton = document.querySelector("#connect");
     var statusDisplay = document.querySelector('#status');
+    var updateButton = document.querySelector('#updateButton');
+    var midiValue1 = document.querySelector('#midi-value-1');
+    var MidiMessageType;
+    (function (MidiMessageType) {
+        MidiMessageType[MidiMessageType["ControlChange"] = 0] = "ControlChange";
+        MidiMessageType[MidiMessageType["ProgramChange"] = 1] = "ProgramChange";
+        MidiMessageType[MidiMessageType["NoteOn"] = 2] = "NoteOn";
+    })(MidiMessageType || (MidiMessageType = {}));
+    var ButtonCommand = /** @class */ (function () {
+        function ButtonCommand(type, value) {
+            this.type = type;
+            this.value = value;
+            this.messageType = type;
+            this.messageValue = value;
+        }
+        return ButtonCommand;
+    }());
+    function sendButtonCommandUpdate(buttonIndex, buttonCommand) {
+        if (!port) {
+            return;
+        }
+        var buttonCommandUsbMessageData = new Uint8Array(3);
+        buttonCommandUsbMessageData[0] = buttonIndex;
+        buttonCommandUsbMessageData[1] = getMidiMessageTypeNumber(buttonCommand.messageType);
+        buttonCommandUsbMessageData[2] = getMidiMessageTypeNumber(buttonCommand.messageValue);
+        port.send(buttonCommandUsbMessageData);
+        console.log('Message sent');
+        console.log(buttonCommandUsbMessageData);
+    }
+    function getMidiMessageTypeNumber(type) {
+        switch (type) {
+            case MidiMessageType.ControlChange: {
+                return parseInt("0xB0");
+                break;
+            }
+            case MidiMessageType.ProgramChange: {
+                return parseInt("0xC0");
+                break;
+            }
+            case MidiMessageType.NoteOn: {
+                return parseInt("0x90");
+                break;
+            }
+        }
+    }
     function connect() {
         port.connect().then(function () {
             statusDisplay.textContent = '';
@@ -75,6 +79,10 @@ window.onload = function () {
                 statusDisplay.textContent = error;
             });
         }
+    });
+    updateButton.addEventListener('click', function () {
+        var midiInputValue1 = midiValue1.value;
+        sendButtonCommandUpdate(0, new ButtonCommand(MidiMessageType.ControlChange, parseInt(midiInputValue1)));
     });
     serial.getPorts().then(function (ports) {
         if (ports.length == 0) {
